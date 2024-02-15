db1d029c5c27b98464e687758d648ab7d5400572
Firgisotya <zzuuddiinn28@gmail.com>
2024-02-15 16:04:43 +0700

    fix: add hashed password in user seeder

--- api/database/seeders/UserSeeder.go
+++ api/database/seeders/UserSeeder.go
@@ -3,6 +3,7 @@ package seeders
 import (
 	"go-blog/api/configs"
 	"go-blog/api/entities"
+	"go-blog/api/securities"
 )
 
 type UserSeeder struct{}
@@ -10,9 +11,11 @@ type UserSeeder struct{}
 func (u *UserSeeder) Run() error {
 	db := configs.DB
 
+	hashedPassword, _ := securities.HashPassword("password")
+
 	users := []entities.User{
-		{RoleID: 1, FirstName: "Admin", LastName: "Admin", Username: "admin", Email: "admin@gmail.com", NoTelp: "08123456789", Photo: "admin.jpg"},
-		{RoleID: 2, FirstName: "User", LastName: "User", Username: "user", Email: "user@gmail.com", NoTelp: "08123456789", Photo: "user.jpg"},
+		{RoleID: 1, FirstName: "Admin", LastName: "Admin", Username: "admin", Email: "admin@gmail.com", NoTelp: "08123456789", Photo: "admin.jpg", Password: string(hashedPassword)},
+		{RoleID: 2, FirstName: "User", LastName: "User", Username: "user", Email: "user@gmail.com", NoTelp: "08123456789", Photo: "user.jpg", Password: string(hashedPassword)},
 	}
 
 	for _, user := range users {
